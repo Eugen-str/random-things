14903467fbd3c3a8de20c7b07a7135301f0a1687
Eugen-str <eugen.01654@gmail.com>
2024-07-17 21:17:58 +0200

    added huffman decoding

--- README.md
+++ README.md
@@ -2,17 +2,25 @@
 
 ### huffman.hs - an implementation of Huffman coding in Haskell
 
-To encode things with it use `huffmanEncode`.
+To encode things use `huffmanEncode x`, where `x` is the string/list to encode.
+
+To decode things use `huffmanDecode encoded ht`, where `encoded` are the encoded bits in a String and `ht` is the HuffmanTable that was used for encoding.
 
 `huffmanTable` returns the encryption table and `huffman` returns the binary tree of the table.
 
-example:
+example, encoding the text "hello world!" and then decoding it with the table and bits:
 
-```console
+```haskell
 $ ghci huffman.hs
-> huffmanEncode "hello world!"
+
+> encoded = huffmanEncode "hello world!"
+> encoded
 "0011100010110111111001010000111011110"
 
-> huffmanTable "aaaaabbbcc"
-[('a',"1"),('b',"01"),('c',"00")]
+> ht = huffmanTable "hello world!"
+> ht
+[('l',"01"),('r',"000"),('h',"001"),('w',"100"),('o',"101"),('e',"1100"),('d',"1101"),('!',"1110"),(' ',"1111")]
+
+> huffmanDecode encoded ht
+"hello world!"
 ```
--- huffman.hs
+++ huffman.hs
@@ -7,17 +7,14 @@ instance (Show a) => Show (Tree a) where
     show (Node Nothing f l r) = "T(" ++ show f ++ ": L" ++ show l ++ ", R" ++ show r ++ ")"
     show (Node (Just v) f _ _) = "N(" ++ show v ++ ": "++ show f ++ ")"
 
-testD = Node (Just 'd') 6 None None
-testE = Node (Just 'e') 5 None None
-
 getElemFreq :: Ord a => [a] -> [(Maybe a, Int)]
 getElemFreq xs = map (\x -> (Just (head x), length x)) $ group $ sort xs
 
 connectTrees :: Tree a -> Tree a -> Tree a
 connectTrees None _ = error "Connecting empty node"
 connectTrees _ None = error "Connecting empty node"
-connectTrees x@(Node _ f1 _ _) y@(Node _ f2 _ _) | f1 > f2 = Node Nothing (f1 + f2) x y
-                                                 | f1 <= f2 = Node Nothing (f1 + f2) y x
+connectTrees x@(Node _ f1 _ _) y@(Node _ f2 _ _) | f1 <= f2 = Node Nothing (f1 + f2) x y
+                                                 | f1 > f2 = Node Nothing (f1 + f2) y x
 
 sortLT :: Tree a -> Tree a -> Ordering
 sortLT x@(Node _ f1 _ _) y@(Node _ f2 _ _) | f1 > f2 = LT
@@ -45,18 +42,49 @@ huffman lst = huffmanIter $ map (\(el, freq) -> Node el freq None None) $ getEle
                 (rest, (a, b)) = leastCommon $ sortBy sortLT xs
                 xs' = sortBy sortLT $ connectTrees a b : rest
 
-huffmanTable :: Ord a => [a] -> [(a, String)]
+newtype HuffmanTable a = HuffmanTable [(a, String)]
+
+binSort :: (Ord a) => (a, String) -> (a, String) -> Ordering
+binSort x y | binX == binY = compare vX vY
+            | length binX == length binY = compare binX binY
+            | otherwise = compare (length binX) (length binY)
+    where
+        binX = snd x
+        binY = snd y
+        vX = fst x
+        vY = fst y
+
+instance (Show a, Ord a) => Show (HuffmanTable a) where
+    show (HuffmanTable xs) = show $ sortBy binSort xs
+
+connectHT :: HuffmanTable a -> HuffmanTable a -> HuffmanTable a
+connectHT (HuffmanTable xs) (HuffmanTable ys) = HuffmanTable (xs ++ ys)
+
+huffmanTable :: Ord a => [a] -> HuffmanTable a
 huffmanTable lst = walkTree (huffman lst) ""
     where
-        walkTree :: Tree a -> String -> [(a, String)]
-        walkTree (Node Nothing _ l r) acc = walkTree l (acc++"1") ++ walkTree r (acc++"0")
-        walkTree (Node (Just v) _ _ _) acc = [(v, acc)]
+        walkTree :: Tree a -> String -> HuffmanTable a
+        walkTree (Node Nothing _ l r) acc = walkTree l (acc++"0") `connectHT` walkTree r (acc++"1")
+        walkTree (Node (Just v) _ _ _) acc = HuffmanTable [(v, acc)]
 
 huffmanEncode :: (Eq a, Ord a) => [a] -> String
-huffmanEncode lst = concatMap (\x -> findCode x huffTable) lst
+huffmanEncode lst = concatMap (`findCode` huffTable) lst
     where
         huffTable = huffmanTable lst
-        findCode x table = if fst curr == x then snd curr else findCode x rest
+        findCode :: Eq a => a -> HuffmanTable a -> String
+        findCode x (HuffmanTable table) = if fst curr == x then snd curr else findCode x rest
             where
                 curr = head table
-                rest = tail table
+                rest = HuffmanTable $ tail table
+
+huffmanDecode :: String -> HuffmanTable a -> [a]
+huffmanDecode bin ht | rest == "" = [decoded]
+                     | otherwise = decoded : huffmanDecode rest ht
+    where
+        (decoded, rest) = huffmanGetNext bin 0 ht
+        huffmanGetNext :: String -> Int -> HuffmanTable a -> (a, String)
+        huffmanGetNext bin n ht@(HuffmanTable table) | length matches == 1 = (fst match, drop n bin)
+                                                     | otherwise = huffmanGetNext bin (n+1) ht
+            where
+                matches = filter (\x -> snd x == take n bin) table
+                match = head matches

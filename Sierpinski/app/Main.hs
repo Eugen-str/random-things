b3900b997ab8617f6732575856e4ceddaac46cbd
Eugen-str <eugen.01654@gmail.com>
2024-07-24 10:10:46 +0200

    changed to more efficient method for sierpinski

--- Sierpinski/app/Main.hs
+++ Sierpinski/app/Main.hs
@@ -10,17 +10,23 @@ bgColor = makeColorI 255 255 255 255
 
 sierpinski :: Int -> Path -> [Picture]
 sierpinski 0 _ = [Blank]
-sierpinski n xs@[a@(ax,ay), b@(bx,by), c@(cx,cy)] = image : (left ++ right ++ top)
+sierpinski n [a@(ax,ay), b@(bx,by), c@(cx,cy)] = image : (left ++ right ++ top)
     where
-        image = lineLoop xs
-        top   = sierpinski (n - 1) [((ax+cx)/2,(ay+cy)/2), ((bx+cx)/2,(by+cy)/2), c]
-        right = sierpinski (n - 1) [(cx,by), b, ((cx+bx)/2,(cy+by)/2)]
-        left  = sierpinski (n - 1) [a, (cx,ay), ((cx+ax)/2,(cy+ay)/2)]
+        h_ac  = ((cx+ax)/2, (cy+ay)/2)
+        h_ab  = (cx, by)
+        h_bc  = ((cx+bx)/2, (cy+by)/2)
+        image = lineLoop [h_ac, h_ab, h_bc]
+        top   = sierpinski (n - 1) [h_ac, h_bc, c]
+        right = sierpinski (n - 1) [h_ab, b, h_bc]
+        left  = sierpinski (n - 1) [a, h_ab, h_ac]
 sierpinski _ _ = error "wtf"
 
 
 pic :: Picture
-pic = pictures $ sierpinski 3 [(-200,-200), (200, -200), (0, 146.41)]
+pic = pictures $ sierpinski 10 initTri
+               ++ [lineLoop initTri]
+    where
+        initTri = [(-200,-200), (200, -200), (0, 146.41)]
 
 main :: IO ()
 main = display win bgColor pic
